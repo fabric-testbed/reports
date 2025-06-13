9c3ef3ea28106946becf51a491832464ea72c2de
Komal Thareja <kthare10@renci.org>
2025-06-13 14:53:58 -0400

    fix import

--- reports_api/swagger_server/models/user_projects.py
+++ reports_api/swagger_server/models/user_projects.py
@@ -5,9 +5,9 @@ from datetime import date, datetime  # noqa: F401
 
 from typing import List, Dict  # noqa: F401
 
-from swagger_server.models.base_model_ import Model
-from swagger_server.models.project import Project  # noqa: F401,E501
-from swagger_server import util
+from reports_api.swagger_server.models.base_model_ import Model
+from reports_api.swagger_server.models.project import Project  # noqa: F401,E501
+from reports_api.swagger_server import util
 
 
 class UserProjects(Model):
